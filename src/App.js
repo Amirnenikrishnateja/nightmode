bb6aef01a8f85c71e03d985d02bb38241e1616f0
Amirnenikrishnateja <krishnatejaamirneni@gmail.com>
2023-09-30 16:20:03 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import articles from './data';
 
 function App() {
+  var [data,setdata]=React.useState(articles)
+  console.log(articles)
+  function dark(){
+    var a=document.getElementById("main")
+    var but=document.getElementById("1")
+    if(a.className==="box1")
+    {
+      a.classList.remove("box1");
+      a.classList.add("night")
+      document.getElementById("n").style.color="white"
+      document.body.style.backgroundColor="black"
+      but.innerHTML="on"
+      console.log(a)
+    }
+    else{
+      a.classList.add("box1");
+      a.classList.remove("night")
+      document.body.style.backgroundColor="white"
+      document.getElementById("n").style.color="black"
+      
+      but.innerHTML="off"
+    }
+    
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <div className='nightmode'>
+      <p id="n">night mode</p>
+      <button onClick={()=>dark()} id="1">off</button>
+      </div>
+      <div className='box1' id="main">
+      {
+        data.map(a=><div>
+          <p>{a.title}</p>
+        <span>{a.length} min read</span>
+          <p>{a.snippet}</p>
+          </div>)
+      }
+      </div>
+      </div>
+  
   );
 }
 
